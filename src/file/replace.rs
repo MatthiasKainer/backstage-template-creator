84b7075686a303cc7f39fb68d2feb1a2e221b587
Matthias Kainer <mail@matthias-kainer.de>
2022-07-15 17:19:31 +0200

    Exclude template.yaml from replacements

--- src/file/replace.rs
+++ src/file/replace.rs
@@ -9,13 +9,10 @@ use log::*;
 
 use walkdir::WalkDir;
 
-fn replace_content(
-    file: &Path,
-    replacements: &Vec<crate::models::Replacement>,
-) -> io::Result<()> {
+fn replace_content(file: &Path, replacements: &Vec<crate::models::Replacement>) -> io::Result<()> {
     let data = match fs::read_to_string(file) {
         Ok(data) => data,
-        Err(_) => return Ok(())
+        Err(_) => return Ok(()),
     };
     let data = replacements.iter().fold(data, |data, replacement| {
         data.replace(&replacement.text, &replacement.with)
@@ -24,19 +21,19 @@ fn replace_content(
     Ok(())
 }
 
-pub fn content(
-    output: &PathBuf,
-    replacements: &Vec<crate::models::Replacement>,
-) -> io::Result<()> {
+pub fn content(output: &PathBuf, replacements: &Vec<crate::models::Replacement>) -> io::Result<()> {
     let walker = WalkDir::new(output).min_depth(1).contents_first(true);
+    let ignore_list = ["template.yml", "template.yaml"];
 
     for entry in walker {
         let entry = entry?;
         let entry_path = entry.path();
 
         if entry_path.is_file() {
-            debug!("Replace content in file {:?}", entry_path);
-            replace_content(entry_path, &replacements)?;
+            if !ignore_list.iter().any(|&e| e == entry_path.clone().file_name().and_then(|s| s.to_str()).unwrap()) {
+                debug!("Replace content in file {:?}", entry_path);
+                replace_content(entry_path, &replacements)?;
+            }
         } else {
             continue;
         }
